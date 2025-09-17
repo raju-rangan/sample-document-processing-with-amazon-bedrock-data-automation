1b9aeaf3d9f5a23c1ba95782eb271e910bfbf267
amitza <aamita00@gmail.com>
2025-09-17 22:54:25 +0300

    refactor(terraform): remove knowledge base S3 bucket output

--- 20-Industry-Use-Cases/23-Mortgage-Application-Processing-Using-Multi-Agent-Collab-Strand-Agents/infrastructure/terraform/outputs.tf
+++ 20-Industry-Use-Cases/23-Mortgage-Application-Processing-Using-Multi-Agent-Collab-Strand-Agents/infrastructure/terraform/outputs.tf
@@ -8,11 +8,6 @@ output "bda_s3_bucket_name" {
     value       = module.bda_s3_bucket.s3_bucket_id
 }
 
-output "kb_s3_bucket_name" {
-    description = "The name of the KB bucket"
-    value       = module.kb_s3_bucket.s3_bucket_id
-}
-
 output "agentcore_iam_role_name" {
     description = "The name of the IAM role for agentcore"
     value       = aws_iam_role.agentcore_role.name
